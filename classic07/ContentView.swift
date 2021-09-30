d3533279107b561cfa109f1e034714989c6e9355
Dujiashan0711 <shandu7652@gmail.com>
2021-09-30 21:31:32 +0800

    Initial Commit

---
+++ classic07/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  classic07
+//
+//  Created by 暨大附中２ on 2021/9/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ classic07/classic07App.swift
@@ -0,0 +1,17 @@
+//
+//  classic07App.swift
+//  classic07
+//
+//  Created by 暨大附中２ on 2021/9/30.
+//
+
+import SwiftUI
+
+@main
+struct classic07App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
