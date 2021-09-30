149b532711cae47b1011ac5e2464d82549f16276
Dujiashan0711 <shandu7652@gmail.com>
2021-09-30 23:34:41 +0800

    版面配置

--- classic07/ContentView.swift
+++ classic07/ContentView.swift
@@ -13,88 +13,133 @@ struct ContentView: View {
     
     var body: some View {
         VStack{
-            Spacer()
-            Spacer()
+            ZStack{
+                Rectangle()
+                    .frame(width: 430, height: 140, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(Color(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)))
+                    .ignoresSafeArea()
             HStack{
             Image(systemName:"gearshape")
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .font(.largeTitle)
+                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                .padding(.bottom,70)
             Text("Setting")
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-        }
-        NavigationView{
-            VStack{
-                HStack{
-            NavigationLink(
-                destination: other(),
-                label: {
-                    Text("系統音量")
-                        .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
-                        .font(.title)
-                }
-            )
-                    Button {
-                        age.number += 1
-                    } label: {
-                        Text("＋")
-                            .font(.title)
-                    }
-                    Button {
-                        age.number -= 1
-                    } label: {
-                        Text("－")
-                            .font(.title)
-                    }
-                }
-                HStack{
-                NavigationLink(
-                    destination: other2(),
-                    label: {
-                        Text("觸控音效")
-                            .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
-                            .font(.title)
-                    }
-                    )
-                    Button {
-                        age.number += 1
-                    } label: {
-                        Text("＋")
-                            .font(.title)
-                    }
-                    Button {
-                        age.number -= 1
-                    } label: {
-                        Text("－")
-                            .font(.title)
-                    }
-                }
-                HStack{
-                NavigationLink(
-                    destination: other3(),
-                    label: {
-                        Text("靈敏程度")
-                            .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
-                            .font(.title)
-                    }
-                    )
-                    Button {
-                        age.number += 1
-                    } label: {
-                        Text("＋")
-                            .font(.title)
-                    }
-                    Button {
-                        age.number -= 1
-                    } label: {
-                        Text("－")
-                            .font(.title)
-                    }
-                }
+                .font(.largeTitle)
+                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                .padding(.bottom,70)
             }
-        }.environmentObject(age)
+            }
+            NavigationView{
+                VStack{
+                    HStack{
+                        Spacer()
+                        NavigationLink(
+                            destination: other(),
+                            label: {
+                                Text("系統音量")
+                                    .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
+                                    .font(.title)
+                            }
+                        )
+                        Spacer()
+                        ZStack{
+                            Rectangle()
+                                .frame(width: 100, height: 50, alignment: .center)
+                                .foregroundColor(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+                                .cornerRadius(16)
+                            HStack{
+                                Button {
+                                    age.number += 1
+                                } label: {
+                                    Text("＋")
+                                        .font(.title)
+                                }
+                                Button {
+                                    age.number -= 1
+                                } label: {
+                                    Text("－")
+                                        .font(.title)
+                                }
+                            }
+                            .frame(width: 100, height: 80, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        }
+                        Spacer()
+                    }
+                    
+                    HStack{
+                        Spacer()
+                        NavigationLink(
+                            destination: other2(),
+                            label: {
+                                Text("觸控音效")
+                                    .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
+                                    .font(.title)
+                            }
+                        )
+                        Spacer()
+                        ZStack{
+                            Rectangle()
+                                .frame(width: 100, height: 50, alignment: .center)
+                                .foregroundColor(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+                                .cornerRadius(16)
+                            HStack{
+                                Button {
+                                    age.number += 1
+                                } label: {
+                                    Text("＋")
+                                        .font(.title)
+                                }
+                                Button {
+                                    age.number -= 1
+                                } label: {
+                                    Text("－")
+                                        .font(.title)
+                                }
+                            }
+                            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 80, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        }
+                        Spacer()
+                    }
+                    HStack{
+                        Spacer()
+                        NavigationLink(
+                            destination: other3(),
+                            label: {
+                                Text("靈敏程度")
+                                    .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
+                                    .font(.title)
+                            }
+                        )
+                        Spacer()
+                        ZStack{
+                            Rectangle()
+                                .frame(width: 100, height: 50, alignment: .center)
+                                .foregroundColor(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+                                .cornerRadius(16)
+                            HStack{
+                                Button {
+                                    age.number += 1
+                                } label: {
+                                    Text("＋")
+                                        .font(.title)
+                                }
+                                Button {
+                                    age.number -= 1
+                                } label: {
+                                    Text("－")
+                                        .font(.title)
+                                }
+                            }
+
+                            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 80, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        }
+                        Spacer()
+                    }
+                }
+            }.environmentObject(age)
         }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
---
+++ classic07/age.swift
@@ -0,0 +1,12 @@
+//
+//  age.swift
+//  classic07
+//
+//  Created by 暨大附中２ on 2021/9/30.
+//
+
+import SwiftUI
+
+class Age : ObservableObject  {
+    @Published var number : Int = 0
+}
